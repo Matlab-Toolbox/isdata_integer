bdc8623070b048f533f43d525d755c87f3ffbeb5
Morgan Prior <morgan@amaras-tech.co.uk>
2014-03-25 20:06:42 +0000

    Improved Test vector usage and added error report line.

--- test/run_isdata_integer_test.m
+++ test/run_isdata_integer_test.m
@@ -7,19 +7,27 @@ addpath('../function/');
 error_count = 0;
 pass_count  = 0;
 
+%% Test vectors
+test ={};
 
-b           = isdata_integer( 2 ) ;
-if (  ~isequal(b, 1)  )
-  disp('isdata_integer Failed ')
-  error_count = error_count + 1;
-else
-  pass_count  = pass_count  + 1;
+%% String input
+test(end+1).input = 2  ;   test(end).expect =  1 ;
+test(end+1).input = 2.5;   test(end).expect =  0 ;
+
+for vector=1:size(test,2)
+   b       = isdata_integer( test(vector).input );
+   if (  ~isequal(b, test(vector).expect)  )
+     disp('isdata_integer() Failed ')
+     error_count = error_count + 1;
+   else
+     pass_count  = pass_count  + 1;
+   end
 end
 
-b           = isdata_integer( 2.5 ) ;
-if (  ~isequal(b, 1)  )
-  disp('isdata_integer Failed ')
-  error_count = error_count + 1;
-else
-  pass_count  = pass_count  + 1;
-end
+%% Test Status Report
+  if error_count > 0
+    disp(['Test FAILED : ', num2str(pass_count), ' passes and ',num2str(error_count),' fails'])
+  else
+    disp(['Test Passed : ', num2str(pass_count), ' checks ran '])
+  end
+
